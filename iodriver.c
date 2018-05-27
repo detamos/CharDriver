3805053facc5a60bbfebb716002d9fdc13b68a62
detamos <deepanshu_tyagi@students.iitmandi.ac.in>
2018-05-27 19:34:08 +0530

    License Added in Module

--- iodriver.c
+++ iodriver.c
@@ -1,5 +1,11 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include "devioctl.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+MODULE_SUPPORTED_DEVICE(DEVICE_FILE);
 
 static int __init load_module(void)
 {
