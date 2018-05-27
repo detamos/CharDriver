d2d0fe6f24165e50e30a47a9a8983738877e769e
detamos <deepanshu_tyagi@students.iitmandi.ac.in>
2018-05-27 19:28:04 +0530

    starting from fresh

--- devioctl.h
+++ devioctl.h
@@ -1,2 +1,11 @@
 #ifndef DEVIOCTL_H
 #define DEVIOCTL_H
+
+#include <linux/ioctl.h>
+
+#define MAJOR_NUM 244
+#define DEVICE_NAME "iodriver"
+#define DEVICE_FILE1 "iitpipe0"
+#define DEVICE_FILE2 "iitpipe1"
+
+#endif
--- iodriver.c
+++ iodriver.c
@@ -1,1 +1,16 @@
-io
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+static int __init load_module(void)
+{
+	printk(KERN_ALERT "Loading the module : iodriver\n");
+	return 0;
+}
+
+static void __exit remove_module(void)
+{
+	printk(KERN_ALERT "Removing the module : iodriver\n");
+}
+
+module_init(load_module);
+module_exit(remove_module);
