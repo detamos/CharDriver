d7cd11783e5770eeff6f4074801109b67f9b81ca
detamos <deepanshu_tyagi@students.iitmandi.ac.in>
2018-05-28 10:39:09 +0530

    controller program

---
+++ control.c
@@ -0,0 +1,69 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include "devioctl.h"
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void ioctl_change_delay(int file_desc,int delay)
+{
+	int ret;
+	ret = ioctl(file_desc,IOCTL_SET_DELAY,delay);
+	if(ret < 0)
+	{
+		printf("ioctl call failed : %d\n",ret);
+		exit(-1);
+	}
+}
+
+char *buffer;
+int len,delay;
+
+int main()
+{
+	buffer = NULL;
+	printf("Enter the size for buffer (in bytes) : ");
+	scanf("%d",&len);
+	printf("Enter the delay (in ms) : ");
+	scanf("%d",&delay);
+	buffer = (char*)malloc(len);
+	if(buffer == NULL)
+	{
+		printf("Can't allocate space for buffer\n");
+		exit(-1);
+	}
+
+	pid_t pid = fork();
+
+	if(pid < 0)
+	{
+		printf("Can't create new process. Exit");
+		exit(-1);
+	}
+
+	int file_desc;
+	if(pid == 0)
+	{
+		file_desc = open("/dev/iitpipe0",0);
+		if(file_desc < 0)
+		{
+			printf("Can't open device file /dev/iitpipe0\n");
+			exit(-1);
+		}
+		close(file_desc);
+	}
+	else
+	{
+		file_desc = open("/dev/iitpipe1",0);
+		if(file_desc < 0)
+		{
+			printf("Can't open device file /dev/iitpipe1\n");
+			exit(-1);
+		}
+		close(file_desc);
+	}
+
+	return 0;
+}
