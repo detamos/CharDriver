4f6702c75d7e2f748bd47adecda93bf427e7d1c8
detamos <deepanshu_tyagi@students.iitmandi.ac.in>
2018-05-29 12:01:41 +0530

    controller program completed

--- control.c
+++ control.c
@@ -20,7 +20,9 @@ void ioctl_change_delay(int file_desc,int delay)
 
 char *buffer;
 int len,delay;
-FILE *inputFile,*outputFile;
+FILE *inFile,*outFile;
+int front = 0,rear = -1,total = 0;
+
 
 int main()
 {
@@ -36,39 +38,138 @@ int main()
 		exit(-1);
 	}
 
-	pid_t pid = fork();
-
-	if(pid < 0)
+	pid_t pid1 = fork();
+	if(pid1 < 0)
 	{
-		printf("Can't create new process. Exit");
+		cout<<"Can't fork new porcess(pid1). Terminating ...\n";
 		exit(-1);
 	}
 
-	if(pid == 0)
+	pid_t pid2 = fork();
+	if(pid1 == 0)
 	{
-	/*	int file_desc = open("iitpipe0",O_RDWR);
-		if(file_desc < 0)
+		if(pid2 < 0)
 		{
-			printf("Can't open device file iitpipe0\n");
+			cout<<"Can't fork new porcess(pid2). Terminating ...\n";
 			exit(-1);
 		}
+		if(pid2 == 0)
+		{
+			inFile = fopen("input","r");
+			if(inFile == NULL)
+			{
+				cout<<"Can't open input file. Terminating ...\n";
+				exit(-1);
+			}
 
-		int numBytes = read(file_desc,buffer,len);
-		printf("Number of bytes read : %d\n",numBytes);
-		close(file_desc);
-	*/
+			char temp[1];
+			int file_desc = open("/dev/iitpipe0",O_WRONLY);
+			if(file_desc == -1)
+			{
+				cout<<"Can't open device file (pid1=0,pid2=0)\n";
+				exit(-1);
+			}
+
+			while(fscanf(inFile,"%c",&temp[0]) == 1)
+			{
+				while(write(file_desc,temp,1,0) != 1);
+			}
+			fclose(inFile);
+		}
+		else
+		{
+			outFile = fopen("output","w");
+			if(outFile == NULL)
+			{
+				cout<<"Can't open output file. Terminating ...\n";
+				exit(-1);
+			}
+
+			int file_desc = open("/dev/iitpipe1",O_RDONLY);
+			if(file_desc == -1)
+			{
+				cout<<"Can't open device file (pid1=0,pid2!=0)\n";
+				exit(-1);
+			}
+
+			char temp[1];
+			while(1)
+			{
+				if(read(file_desc,temp,1,0) != 1)
+				{
+					delay(1000);
+					if(read(file_desc,temp,1,0) != 1)
+						break;
+				}
+				fprintf(outFile,"%c",temp[0]);
+			}
+
+			fclose(outFile);
+			close(file_desc);
+		}
 	}
 	else
 	{
-		int file_desc = open("iitpipe1",O_RDWR);
-		if(file_desc < 0)
+		if(pid2 < 0)
 		{
-			printf("Can't open device file iitpipe1\n");
+			cout<<"Can't fork new porcess(pid2(child)). Terminating ...\n";
 			exit(-1);
 		}
-		strcpy(buffer,"Hello World!");
-		int numBytes = write(file_desc,buffer,len);
-		close(file_desc);
+		if(pid2 == 0)
+		{
+			int file_desc = open("/dev/iitpipe0",O_RDONLY);
+			if(file_desc == -1)
+			{
+				cout<<"Can't open device file (pid1=0,pid2!=0)\n";
+				exit(-1);
+			}
+
+			char temp[1];
+			while(1)
+			{
+				if(read(file_desc,temp,1,0) != 1)
+				{
+					delay(1000);
+					if(read(file_desc,temp,1,0) != 1)
+						break;
+				}
+				if(total == len)
+					continue;
+				if(rear == len-1)
+				{
+					rear = -1;
+				}
+				buffer[++rear] = temp[0];
+				total ++;
+			}
+
+			close(file_desc);
+		}
+		else
+		{
+			int file_desc = open("/dev/iitpipe1",O_WRONLY);
+			if(file_desc == -1)
+			{
+				cout<<"Can't open device file (pid1=0,pid2!=0)\n";
+				exit(-1);
+			}
+
+			char temp[1];
+			while(1)
+			{
+				if(total == 0)
+				{
+					delay(1000);
+					if(total == 0)
+						break;
+				}
+				temp[0] = buffer[front++];
+				if(front == len)
+					front = 0;
+				total --;
+				int written = write(file_desc,temp,1,0);
+			}
+		}
 	}
 
 	return 0;
