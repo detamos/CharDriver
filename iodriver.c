7a27d73cdbe64c02a7096d8e28aca2ae8d27c9e0
detamos <deepanshu_tyagi@students.iitmandi.ac.in>
2018-06-02 15:28:08 +0530

    hello

--- iodriver.c
+++ iodriver.c
@@ -52,7 +52,7 @@ ssize_t device_write(struct file *filp,const char *buffer,size_t len,loff_t *off
 	while(total2 != MAX && len)
 	{
 		int ret = copy_from_user(tempData2,buffer++,1);
-		printk(KERN_INFO "READ : %d\n",tempData2);
+		printk(KERN_INFO "READ : %p\n",tempData2);
 		if(rear2 == MAX-1)
 			rear2 = -1;
 		msg2[++rear2] = tempData2[0];
